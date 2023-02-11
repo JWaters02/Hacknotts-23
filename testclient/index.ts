9a8afc8129c18cf6dd08ff0970b3ed63701e79d0
Joe <burtonjae@hotmail.co.uk>
2023-02-11 16:49:17 +0000

    Change testclient to use websockets

---
+++ testclient/index.ts
@@ -0,0 +1,24 @@
+
+import { WebSocket } from 'ws';
+import {MessageType} from '../server/Server'
+
+const PORT = 8080
+
+const ws = new WebSocket(`ws://localhost:${PORT}`);
+ws.onopen = event => {
+    ws.send(JSON.stringify({type: MessageType.CreateSession}));
+};
+ws.onmessage = event => {
+    const data = JSON.parse(event.data);
+    switch (data.type) {
+        case MessageType.CreateSession:
+            const id = data.id;
+            console.log(`Session id is ${id}`)
+            const ws2 = new WebSocket(`ws://localhost:${PORT}`);
+            ws2.onopen = event => {
+                ws2.send(JSON.stringify({type: MessageType.JoinSession, id}));
+                console.log(`Joined with a second client`);
+            };
+            break;
+    }
+};
