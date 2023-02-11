fdbaddb1362e2daff1c3cd3e9d84109e64ccabd5
Fahd <fahd31256@gmail.com>
2023-02-11 15:32:38 +0000

    add server skeleton

--- server/Server.ts
+++ server/Server.ts
@@ -1,6 +1,17 @@
 import { WebSocket } from 'ws';
 const express = require('express')
 
+enum MessageType {
+    Obstacle,
+    Submit,
+    Example
+};
+
+interface ClientMessage {
+    type: MessageType,
+    data: Object
+};
+
 export class Server {
     private socket: WebSocket; // The main server websocket
 
@@ -9,6 +20,7 @@ export class Server {
             port: "" + port
         });
         this.socket.on('connection', this.onClientConnect.bind(this));
+        this.socket.on('message', this.onClientMessage.bind(this));
 
         const app = express(); // The http server
         app.get('/createSession', this.onSessionCreate.bind(this));
@@ -16,15 +28,39 @@ export class Server {
         app.listen(port + 1, () => { console.log('Listening on port ' + port) });
     }
 
+    // HTTP API Methods
     private onClientConnect(clientSocket: WebSocket) {
         console.log('New Client Connected!');
     }
 
+    // This function is called initially, once the 
     private onSessionCreate(req: Object, res: Object) {
+        console.log('Here, the session will be created');
         console.log(req);
         console.log(res);
     }
 
+    /*
+        This function is called when the user sends a message to the server that contains
+        either like a powerup or send an obstacle to the server. They could also be sending
+        some code to be run by the server, or even asking for a new challenge or whatever.
+        Essentially, any in-game communication between the server and the client will be
+        handled inside this function.
+    */
+    private onClientMessage(req: Object, res: Object) {
 
+    }
+
+    private runExample(req, res, clientSocket) {
+
+    }
+
+    private sendObstacle(req, res, clientSocket) {
+
+    }
+
+    private submit(req, res, clientSocket) {
+
+    }
 
 }
